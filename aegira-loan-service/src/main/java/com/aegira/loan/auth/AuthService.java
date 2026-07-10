308684533b616f971e60e7674b0cfd1e6e1ddd1d
fadka23 <fadhlyandriawan@gmail.com>
2026-07-10 18:45:29 +0700

    Submit 1

--- aegira-loan-service/src/main/java/com/aegira/loan/auth/AuthService.java
+++ aegira-loan-service/src/main/java/com/aegira/loan/auth/AuthService.java
@@ -25,51 +25,38 @@ public class AuthService {
 
     public LoginResponse login(LoginRequest request) {
 
-        
         // User user = userRepository.findByEmail(request.getEmail())
-        //         .orElseThrow(() -> new BadRequestException("Invalid credentials"));
+        // .orElseThrow(() -> new BadRequestException("Invalid credentials"));
 
-
-
-        // if (!passwordEncoder.matches(request.getPassword(), user.getPasswordHash())) {
-        //     throw new BadRequestException("Invalid credentials");
+        // if (!passwordEncoder.matches(request.getPassword(), user.getPasswordHash()))
+        // {
+        // throw new BadRequestException("Invalid credentials");
         // }
-        // return new LoginResponse(jwtTokenProvider.generate(user), user.getId(), user.getEmail(), user.getRole());
-    
-
-
+        // return new LoginResponse(jwtTokenProvider.generate(user), user.getId(),
+        // user.getEmail(), user.getRole());
 
         Optional<User> userOpt = userRepository.findByEmail(request.getEmail());
-        if(userOpt.isEmpty()){
+        if (!userOpt.isPresent()) {
             log.warn("event_name=auth_login_failed error_code=BAD_REQUEST");
             throw new BadRequestException("invalid credential");
         }
 
-
         User user = userOpt.get();
         if (!passwordEncoder.matches(request.getPassword(), user.getPasswordHash())) {
-            log.warn("event_name=auth_login_failed user_id={} role={} error_code=BAD_REQUEST", user.getId(), user.getRole());
+            log.warn("event_name=auth_login_failed user_id={} role={} error_code=BAD_REQUEST", user.getId(),
+                    user.getRole());
             throw new BadRequestException("Invalid credentials");
         }
 
-
-
         String token = jwtTokenProvider.generate(user);
 
-        LoginResponse response = new LoginResponse(token , user.getId(), user.getEmail(), user.getRole());
+        LoginResponse response = new LoginResponse(token, user.getId(), user.getEmail(), user.getRole());
 
         log.info("event_name=auth_login_success user_id={} role={}", user.getId(), user.getRole());
 
         return response;
 
-        
-    
-
-    
     }
-
-
-
 
     public LoginResponse me() {
         User user = securityUtil.currentUser();
--- aegira-loan-service/src/main/java/com/aegira/loan/auth/CustomUserDetailsService.java
+++ aegira-loan-service/src/main/java/com/aegira/loan/auth/CustomUserDetailsService.java
@@ -38,7 +38,6 @@ public class CustomUserDetailsService implements UserDetailsService {
                 true,
                 true,
                 true,
-                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
-        );
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name())));
     }
 }
